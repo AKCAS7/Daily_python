ac9db1064b11b2b93ec1c75d66e03db4cc3bd089
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-02-01 22:58:21 +0530

    Update read_csv_data.py

--- read_csv_data.py
+++ read_csv_data.py
@@ -1,10 +1,12 @@
 import csv
 with open("weather_data.csv") as file:
-    contents = csv.reader(file)
+    contents = []
+    read = csv.reader(file)
     for row in file:
         contents.append(row)
         
 print(contents)
         
+
 
 
