607ca3728a21b317351f502f34476c2c0e9aac94
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-03-02 01:46:03 +0530

    Update Dictionary_comprehension.py

--- Dictionary_comprehension.py
+++ Dictionary_comprehension.py
@@ -5,3 +5,7 @@
 import random
 names = ['Alex', 'Dave', 'James', 'Gabriel']
 student_scores = { name: random.randint(1,100) for name in names }
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = { word:len(word) for word in sentence.split()}
+print(result)
